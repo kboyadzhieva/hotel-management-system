dbbab55c5f6eeff4e8bac58086474440fb6a0333
kboyadzhieva <>
2023-01-24 14:11:25 +0200

    J1-17: Create Room DTOs

---
+++ src/main/java/com/moonlighthotel/hotelmanagementsystem/dto/room/request/RoomRequest.java
@@ -0,0 +1,53 @@
+package com.moonlighthotel.hotelmanagementsystem.dto.room.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class RoomRequest {
+
+    @Length(min = 2, max = 255)
+    @NotBlank(message = "A title is required. Please, enter a title of the room.")
+    private String title;
+
+    @NotBlank(message = "An Image is required. Please, add an image.")
+    private String image;
+
+    @NotNull(message = "Images are required. Please, add images.")
+    private Set<String> images;
+
+    @NotBlank(message = "Please, enter a description of the room.")
+    @Length(min = 2, max = 255)
+    private String description;
+
+    @NotBlank(message = "Please, enter the facilities of the room.")
+    @Length(min = 2, max = 255)
+    private String facilities;
+
+    @NotNull(message = "Please, enter the room area.")
+    @Positive(message = "The value must be greater than 0.")
+    private Double area;
+
+    @NotNull(message = "Please, enter the maximum number of guests.")
+    @Positive(message = "The value must be greater than 0.")
+    private Integer people;
+
+    @NotNull(message = "Please, enter a price per night.")
+    @Positive(message = "The value must be greater than 0.")
+    private Double price;
+
+    @NotNull(message = "Please, enter the total number of rooms of that type.")
+    @Positive(message = "The value must be greater than 0.")
+    private Integer count;
+}
