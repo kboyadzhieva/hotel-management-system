fdefa263de60eba0c0a63707af8617a4de453da7
kboyadzhieva <>
2023-01-26 11:37:56 +0200

    J1-21: Implement delete room by id endpoint

--- src/main/java/com/moonlighthotel/hotelmanagementsystem/controller/RoomController.java
+++ src/main/java/com/moonlighthotel/hotelmanagementsystem/controller/RoomController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -36,5 +33,12 @@ public class RoomController {
         RoomResponse roomResponse = roomConverter.toRoomResponse(savedRoom);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(roomResponse);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Long id) {
+        roomService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
