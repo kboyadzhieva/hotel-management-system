33ec3a567a954fe3ddf395180fe4d960a65ec21e
kboyadzhieva <>
2023-01-26 22:31:24 +0200

    J1-23: Create RoomReservation DTOs

---
+++ src/main/java/com/moonlighthotel/hotelmanagementsystem/dto/roomreservation/request/RoomReservationRequestUpdate.java
@@ -0,0 +1,49 @@
+package com.moonlighthotel.hotelmanagementsystem.dto.roomreservation.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class RoomReservationRequestUpdate {
+
+    @NotNull(message = "The user is required.")
+    private Long user;
+
+    @NotBlank(message = "Please enter start date")
+    private String startDate;
+
+    @NotBlank(message = "Please enter end date")
+    private String endDate;
+
+    @NotNull(message = "Please enter a number of adults.")
+    @Min(value = 1)
+    @Max(value = 4)
+    private Integer adults;
+
+    @NotNull(message = "Please enter a number of kids.")
+    @Min(value = 0)
+    @Max(value = 4)
+    private Integer kids;
+
+    @NotBlank(message = "Bed type choice is required")
+    private String typeBed;
+
+    @NotBlank(message = "View type choice is required")
+    private String view;
+
+    @NotNull(message = "Please enter a price.")
+    private Double price;
+
+    @NotNull(message = "Please select a reservation status.")
+    private Integer status;
+}
---
+++ src/main/java/com/moonlighthotel/hotelmanagementsystem/dto/roomreservation/response/RoomReservationResponse.java
@@ -0,0 +1,41 @@
+package com.moonlighthotel.hotelmanagementsystem.dto.roomreservation.response;
+
+import com.moonlighthotel.hotelmanagementsystem.dto.room.response.RoomResponse;
+import com.moonlighthotel.hotelmanagementsystem.dto.user.response.UserResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class RoomReservationResponse {
+
+    private Long id;
+
+    private String startDate;
+
+    private String endDate;
+
+    private Integer days;
+
+    private Integer adults;
+
+    private Integer kids;
+
+    private String typeBed;
+
+    private String view;
+
+    private Double price;
+
+    private String created;
+
+    private String status;
+
+    private RoomResponse room;
+
+    private UserResponse user;
+}
