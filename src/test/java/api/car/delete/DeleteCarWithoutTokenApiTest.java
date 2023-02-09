0c32b51a56b3bda059c9c275ff848d1152a1575a
kboyadzhieva <>
2023-02-09 15:44:31 +0200

    J1-38: Delete car without token API test

---
+++ src/test/java/api/car/delete/DeleteCarWithoutTokenApiTest.java
@@ -0,0 +1,58 @@
+package api.car.delete;
+
+import api.BaseApiTest;
+import api.helper.creator.CarCreator;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.request.CarRequest;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.response.CarResponse;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+
+@RunWith(JUnit4.class)
+public class DeleteCarWithoutTokenApiTest extends BaseApiTest {
+
+    private static final String URI = "/cars";
+    private final CarCreator carCreator = new CarCreator();
+
+    @Test
+    public void deleteCarWithoutTokenShouldReturnUnauthorized() {
+        Long id = saveCarBeforeTest();
+
+        given()
+                .when()
+                .pathParam("id", id)
+                .delete(URI + "/{id}")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+
+        deleteCarAfterTest(id);
+    }
+
+    private Long saveCarBeforeTest() {
+        CarRequest car = carCreator.createCar();
+
+        CarResponse carResponse =
+                getClientWithAdminToken()
+                        .contentType(ContentType.JSON)
+                        .body(car)
+                        .when()
+                        .post(URI)
+                        .then()
+                        .extract()
+                        .as(CarResponse.class);
+
+        return carResponse.getId();
+    }
+
+    private void deleteCarAfterTest(Long id) {
+        getClientWithAdminToken()
+                .when()
+                .pathParam("id", id)
+                .delete(URI + "/{id}");
+    }
+}
