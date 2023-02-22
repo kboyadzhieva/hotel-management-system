4c2452da7e51de2d8c2e19585277958eb19b7657
kboyadzhieva <>
2023-02-23 00:01:35 +0200

    J2-06: Update car transfer with client token API test

--- src/test/java/api/helper/updater/CarTransferUpdater.java
+++ src/test/java/api/helper/updater/CarTransferUpdater.java
@@ -8,12 +8,12 @@ public class CarTransferUpdater {
     private final ObjectCreator objectCreator = new ObjectCreator();
 
     public CarTransferRequest updateCarTransfer() {
-        String content = "classpath:createCarTransfer.json";
+        String content = "classpath:updateCarTransfer.json";
         return objectCreator.createObject(content, CarTransferRequest.class);
     }
 
     public CarTransferRequest updateCarTransferWithInvalidData() {
-        String content = "classpath:createCarTransferWithInvalidData.json";
+        String content = "classpath:updateCarTransferWithInvalidData.json";
         return objectCreator.createObject(content, CarTransferRequest.class);
     }
 }
---
+++ src/test/java/api/transfer/update/UpdateCarTransferWithClientTokenApiTest.java
@@ -0,0 +1,84 @@
+package api.transfer.update;
+
+import api.BaseApiTest;
+import api.helper.creator.CarCreator;
+import api.helper.creator.CarTransferCreator;
+import api.helper.updater.CarTransferUpdater;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.request.CarRequest;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.response.CarResponse;
+import com.moonlighthotel.hotelmanagementsystem.dto.transfer.request.CarTransferRequest;
+import com.moonlighthotel.hotelmanagementsystem.dto.transfer.response.CarTransferResponse;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.http.HttpStatus;
+
+@RunWith(JUnit4.class)
+public class UpdateCarTransferWithClientTokenApiTest extends BaseApiTest {
+
+    private static final String URI = "/cars";
+    private final CarCreator carCreator = new CarCreator();
+    private final CarTransferCreator carTransferCreator = new CarTransferCreator();
+    private final CarTransferUpdater carTransferUpdater = new CarTransferUpdater();
+
+    @Test
+    public void updateCarTransferWithClientTokenShouldReturnForbidden() {
+        Long id = saveCarBeforeTest();
+        Long tid = saveCarTransferBeforeTest(id);
+        CarTransferRequest updatedCarTransfer = carTransferUpdater.updateCarTransfer();
+
+        getClientWithClientToken()
+                .contentType(ContentType.JSON)
+                .body(updatedCarTransfer)
+                .when()
+                .pathParam("id", id)
+                .pathParam("tid", tid)
+                .put(URI + "/{id}/transfers/{tid}")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+
+        deleteCarAfterTest(id);
+    }
+
+    private Long saveCarBeforeTest() {
+        CarRequest car = carCreator.createCar();
+
+        CarResponse carResponse =
+                getClientWithAdminToken()
+                        .contentType(ContentType.JSON)
+                        .body(car)
+                        .when()
+                        .post(URI)
+                        .then()
+                        .extract()
+                        .as(CarResponse.class);
+
+        return carResponse.getId();
+    }
+
+    private Long saveCarTransferBeforeTest(Long id) {
+        CarTransferRequest carTransfer = carTransferCreator.createCarTransfer();
+
+        CarTransferResponse carTransferResponse =
+                getClientWithAdminToken()
+                        .contentType(ContentType.JSON)
+                        .body(carTransfer)
+                        .when()
+                        .pathParam("id", id)
+                        .post(URI + "/{id}/transfers")
+                        .then()
+                        .extract()
+                        .as(CarTransferResponse.class);
+
+        return carTransferResponse.getId();
+    }
+
+    private void deleteCarAfterTest(Long id) {
+        getClientWithAdminToken()
+                .when()
+                .pathParam("id", id)
+                .delete(URI + "/{id}");
+    }
+}
