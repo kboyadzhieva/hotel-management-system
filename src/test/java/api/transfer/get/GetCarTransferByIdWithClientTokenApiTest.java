8ec1282d94ab4ef3c942662ae1140bb198956953
kboyadzhieva <>
2023-02-22 23:27:01 +0200

    J2-05: Get car transfer by ID with client token API test

---
+++ src/test/java/api/transfer/get/GetCarTransferByIdWithClientTokenApiTest.java
@@ -0,0 +1,79 @@
+package api.transfer.get;
+
+import api.BaseApiTest;
+import api.helper.creator.CarCreator;
+import api.helper.creator.CarTransferCreator;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.request.CarRequest;
+import com.moonlighthotel.hotelmanagementsystem.dto.car.response.CarResponse;
+import com.moonlighthotel.hotelmanagementsystem.dto.transfer.request.CarTransferRequest;
+import com.moonlighthotel.hotelmanagementsystem.dto.transfer.response.CarTransferResponse;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.http.HttpStatus;
+
+@RunWith(JUnit4.class)
+public class GetCarTransferByIdWithClientTokenApiTest extends BaseApiTest {
+
+    private static final String URI = "/cars";
+    private final CarCreator carCreator = new CarCreator();
+    private final CarTransferCreator carTransferCreator = new CarTransferCreator();
+
+    @Test
+    public void getCarTransferByIdWithClientTokenShouldReturnForbidden() {
+        Long id = saveCarBeforeTest();
+        Long tid = saveCarTransferBeforeTest(id);
+
+        getClientWithClientToken()
+                .when()
+                .pathParam("id", id)
+                .pathParam("tid", tid)
+                .get(URI + "/{id}/transfers/{tid}")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+
+        deleteCarAfterTest(id);
+    }
+
+    private Long saveCarBeforeTest() {
+        CarRequest car = carCreator.createCar();
+
+        CarResponse carResponse =
+                getClientWithAdminToken()
+                        .contentType(ContentType.JSON)
+                        .body(car)
+                        .when()
+                        .post(URI)
+                        .then()
+                        .extract()
+                        .as(CarResponse.class);
+
+        return carResponse.getId();
+    }
+
+    private Long saveCarTransferBeforeTest(Long id) {
+        CarTransferRequest carTransfer = carTransferCreator.createCarTransfer();
+
+        CarTransferResponse carTransferResponse =
+                getClientWithAdminToken()
+                        .contentType(ContentType.JSON)
+                        .body(carTransfer)
+                        .when()
+                        .pathParam("id", id)
+                        .post(URI + "/{id}/transfers")
+                        .then()
+                        .extract()
+                        .as(CarTransferResponse.class);
+
+        return carTransferResponse.getId();
+    }
+
+    private void deleteCarAfterTest(Long id) {
+        getClientWithAdminToken()
+                .when()
+                .pathParam("id", id)
+                .delete(URI + "/{id}");
+    }
+}
