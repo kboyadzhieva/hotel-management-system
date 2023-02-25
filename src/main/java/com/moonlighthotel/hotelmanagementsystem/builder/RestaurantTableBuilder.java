c14c3b9ad2ab089ef6041629277ef26a1b27641f
kboyadzhieva <>
2023-02-25 23:12:36 +0200

    J3-02: Implement a restaurant table service layer

---
+++ src/main/java/com/moonlighthotel/hotelmanagementsystem/builder/RestaurantTableBuilder.java
@@ -0,0 +1,19 @@
+package com.moonlighthotel.hotelmanagementsystem.builder;
+
+import com.moonlighthotel.hotelmanagementsystem.enumeration.SectionType;
+import com.moonlighthotel.hotelmanagementsystem.model.restaurant.RestaurantTable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestaurantTableBuilder {
+
+    public RestaurantTable build(RestaurantTable restaurantTable, SectionType sectionType) {
+        return RestaurantTable.builder()
+                .id(restaurantTable.getId())
+                .zone(restaurantTable.getZone())
+                .number(restaurantTable.getNumber())
+                .people(restaurantTable.getPeople())
+                .smoking(sectionType)
+                .build();
+    }
+}
